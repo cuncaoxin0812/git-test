4ebabfbc5c56a8fb25ec76b1dd88e6188299620f
hugh <hugh@qq.com>
2021-08-22 16:51:16 +0800

    第一次提交

---
+++ src/com/hugh/gittest/GitTest.java
@@ -0,0 +1,14 @@
+package com.hugh.gittest;
+
+/**
+ * @Description:
+ * @author: HUGH_HU
+ * @create: 2021-08-22 16:47
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+}
